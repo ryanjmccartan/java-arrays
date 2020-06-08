167732adc8f80a203f3f828e84c68dae2ec7e380
Ryan McCartan <ryanjmccartan@gmail.com>
2020-06-08 15:48:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Learning Java
+
+Gaining experience with learning how to write arrays and array lists using Java.
---
+++ src/Array.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) throws Exception {
+        String[] words = {"Learning", "Java", "Is", "So", "Fun"};
+
+        System.out.println(Arrays.toString(words));
+    }
+}
